8f58c6964906afdf22ac967365dc1b376d1a9ba6
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-12-09 00:03:53 +0530

    #1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,35 @@
-function ExpenseItem(){
-    return (<div>
-        <h2>Expense Items</h2>
-        <p>Food 10rs</p>
-        <p>Petrol 10rs</p>
-        <p>Movies 200rs</p>
-    </div>)
+import "./ExpenseItem.css";
+function ExpenseItem() {
+  const date = new Date();
+  const title = "Car";
+  const amt = 3.3;
+  const location = "India";
+  return (
+    <div>
+      <div className="expense-item">
+        <div>{date.toDateString()}</div>
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <h2>{location}</h2>
+          <div className="expense-item__price">{amt}</div>
+        </div>
+      </div>
+      <div className="expense-item">
+        <div>{date.toDateString()}</div>
+        <div className="expense-item__description">
+          <h2>{title} </h2>
+          <div className="expense-item__price">{amt}</div>
+        </div>
+      </div>
+      <div className="expense-item">
+        <div>{date.toDateString()}</div>
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{amt}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
