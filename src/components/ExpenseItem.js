4c0bfceea0e5ef407c3824f93802cf53ace9b513
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-12-08 23:42:46 +0530

    init commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return (<div>
+        <h2>Expense Items</h2>
+        <p>Food 10rs</p>
+        <p>Petrol 10rs</p>
+        <p>Movies 200rs</p>
+    </div>)
+}
+
+export default ExpenseItem
