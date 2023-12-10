f8b6c6386970403cc3c88abfcbf5c5375597be85
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-12-10 21:17:31 +0530

    #5

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,26 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = (e) => {
+    const id = e.target.id;
+
+    const expenseItem = e.target.closest(".expense-item");
+
+    if (expenseItem) {
+      expenseItem.remove();
+      console.log("ExpenseItem removed:", id);
+    }
+  };
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.id}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler} id={props.id}>
+        Change Title
+      </button>
     </Card>
   );
 };
