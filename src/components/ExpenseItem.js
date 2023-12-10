21bafa0dacf42c6343ff44f7eb432608dab5a8c1
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-12-10 18:22:21 +0530

    #3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 function ExpenseItem(props) {
   return (
     <div>
       <div className="expense-item">
-        <div>{props.date.toDateString()}</div>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <h2>{props.location}</h2>
-          <div className="expense-item__price">{props.amt}</div>
-        </div>
+        <ExpenseDate date={props.date}></ExpenseDate>
+
+        <ExpenseDetails
+          title={props.title}
+          location={props.location}
+          amt={props.amt}
+        />
       </div>
     </div>
   );
