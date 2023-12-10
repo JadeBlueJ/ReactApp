37b813d8a50e86c838e87e93824417c3aed7a7b5
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-12-10 19:25:31 +0530

    #4

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
   const expenses = [
-    { date: new Date(), title: "Car", amt: 66, location: "India" },
-    { date: new Date(), title: "Bike", amt: 45, location: "USA" },
-    { date: new Date(), title: "Rapido", amt: 334, location: "Mexico" },
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amt={expenses[0].amt}
-        location={expenses[0].location}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amt={expenses[1].amt}
-        date={expenses[1].date}
-        location={expenses[1].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amt={expenses[2].amt}
-        date={expenses[2].date}
-        location={expenses[2].location}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
